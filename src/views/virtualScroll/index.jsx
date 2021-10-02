bfee855c301040eb30184922eb4491877010d63e
ljl <776745393@qq.com>
2021-10-02 11:56:32 +0800

    feat(virtual): scroll

---
+++ src/views/virtualScroll/index.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Index extends Component {
+    render() {
+        return (
+            <div>
+                2312
+            </div>
+        );
+    }
+}
+
+export default Index;

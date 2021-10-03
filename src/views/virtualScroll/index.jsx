670f4629f7c867092f81ec8e0beb58e6f9f34b37
ljl <776745393@qq.com>
2021-10-03 20:45:52 +0800

    feat(virtual)

--- src/views/virtualScroll/index.jsx
+++ src/views/virtualScroll/index.jsx
@@ -1,10 +1,45 @@
 import React, {Component} from 'react';
+import './index.css'
+
+let list = []
+for (let i = 0; i < 10000; i++) {
+    list.push(i)
+}
 
 class Index extends Component {
+    state = {
+        height: 60,
+        viewList: list.slice(0, 40),
+        offsetY: 0
+    }
+    handleScroll = (e) => {
+        let sliceNum = Math.floor(e.target.scrollTop / this.state.height)
+        this.setState({
+            offsetY: e.target.scrollTop,
+            viewList: list.slice(sliceNum, sliceNum + 40)
+        })
+    }
+
+    componentDidMount() {
+    }
+
     render() {
+        const {height, viewList, offsetY} = this.state
         return (
-            <div>
-                2312
+            <div className='box' onScroll={this.handleScroll}>
+                <ul>
+                    {
+                        viewList.map((item) => {
+                            return <li
+                                style={{
+                                    height: `${height}px`,
+                                    lineHeight: `${height}px`,
+                                    transform: `translateY(${offsetY}px)`
+                                }}
+                                key={item}>{item + 1}</li>
+                        })
+                    }
+                </ul>
             </div>
         );
     }
